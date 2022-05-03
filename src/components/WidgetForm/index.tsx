8f1ad1b1eb45ba22da277e344cc8d4b3ad5ba0f0
Yves de Sá <yvesdsb98@gmail.com>
2022-05-03 19:26:06 -0300

    feat: created the form component for sending feedback

---
+++ src/components/WidgetForm/index.tsx
@@ -0,0 +1,60 @@
+import { CloseButton } from "../CloseButton";
+
+import bugImage from "../../assets/bug.svg";
+import ideaImage from "../../assets/idea.svg";
+import thoughtImage from "../../assets/thought.svg";
+import { useState } from "react";
+import { FeedbackTypeStep } from "./Steps/FeedbackTypeStep";
+import { FeedbackContentStep } from "./Steps/FeedbackContentStep";
+
+export const FeedbackTypes = {
+  BUG: {
+    title: "Problema",
+    image: {
+      source: bugImage,
+      alt: "imagem de um inseto"
+    }
+  },
+  IDEA: {
+    title: "Ideia",
+    image: {
+      source: ideaImage,
+      alt: "imagem de um lâmpada"
+    }
+  },
+  OTHER: {
+    title: "Outro",
+    image: {
+      source: thoughtImage,
+      alt: "imagem de uma nuvem de pensamento"
+    }
+  },
+}
+
+export type FeedbackType = keyof typeof FeedbackTypes;
+
+export function WidgetForm() {
+  const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null);
+
+  function handleRestartFeedback() {
+    setFeedbackType(null);
+  }
+
+  return (
+    <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto" >
+
+      {!feedbackType ? (
+        <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType} />
+      ) : (
+        <FeedbackContentStep
+          onFeedbackRestartRequested={handleRestartFeedback}
+          feedbackType={feedbackType}
+        />
+      )}
+
+      <footer className="text-xs text-neutral-400">
+        Feito com ♥ pela <a className="underline underline-offset-2" href="https://www.rocketseat.com.br/">Rocketseat</a>
+      </footer>
+    </div>
+  )
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,10 +4,17 @@ module.exports = {
     extend: {
       colors: {
         brand: {
-          500: '#8357e6'
+          300: '#996DFF',
+          500: '#8357E6'
+        },
+        borderRadius: {
+          md: '4px'
         }
       }
     },
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('tailwind-scrollbar'),
+  ],
 }
